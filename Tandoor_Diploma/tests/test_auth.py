e0708deb9291c4be6118186560cb40e1599624ab
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2026-06-07 16:53:34 +0300

    Delete Tandoor_Diploma directory

--- Tandoor_Diploma/tests/test_auth.py
+++
@@ -1,4 +0,0 @@
-def test_documentation():
-    """UI tests not automated due to test server limitations (ERR_CONNECTION_RESET).
-    Manual testing of all UI scenarios was performed successfully."""
-    assert True
