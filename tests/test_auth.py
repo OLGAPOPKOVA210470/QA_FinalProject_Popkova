6c4cc685c53d781a8e45375fa39ac73aaf61aba4
Olga Popkova <popkovaolga1970@gmail.com>
2026-06-13 10:42:32 +0300

    Remove test_auth.py stub

--- tests/test_auth.py
+++
@@ -1,5 +0,0 @@
-'@pytest.mark.ui'  
-def test_documentation():
-    """UI tests not automated due to test server limitations (ERR_CONNECTION_RESET).
-    Manual testing of all UI scenarios was performed successfully."""
-    assert True
